5e8ae3d339dabd2efdcd9f5fd2ed938495409203
yamat47 <yamat47.thirddown@gmail.com>
2024-05-31 10:35:08 +0900

    Remove specs only on forked repository.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 require "bundler/setup"
 require "pgdump_scrambler"
-require File.expand_path('../spec/dummy/config/environment', __dir__)
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
